25061f31d66acc1808d6ad8e17e34fd096b61d5a
Daniel Ale <danielale9291@gmail.com>
2023-02-19 21:55:41 +0100

    feat: update records

---
+++ readme.md
@@ -0,0 +1,22 @@
+## How to generate or update the py files for a proto 
+```shell
+cd retailcore_proto
+py -m grpc_tools.protoc --proto_path=./proto --python_out=./py_grpc --pyi_out=./py_grpc --grpc_python_out=./py_grpc ./proto/user.proto
+```
+
+### After the file must have been generated, navigate to py_grpc folder and make the following edit
+
+Old [protoName]_pb2_grpc.py as [protoName]__pb2_grpc.py
+```python
+import user_pb2 as user__pb2
+```
+
+New [protoName]_pb2 as [protoName]__pb2
+```python
+from retailcore_proto.py_grpc import user_pb2 as user__pb2
+```
+
+Finally, update the version number in retailcore_proto/__init__.py by increasing using the right semantic naming convention
+```python
+__version__ = '0.0.x'
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+import re
+from setuptools import find_packages, setup
+
+with open('retailcore_proto/__init__.py', 'rb') as f:
+    version = str(eval(re.search(r'__version__\s+=\s+(.*)', f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='retailcore_proto',
+    version=version,
+    description='RetailCore Shared Proto',
+    url='https://github.com/Prunedge-Dev-Team/retailcore-shared-proto',
+    author='Daniel Ale',
+    author_email='daniel.ale@prunedge.com',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.8",
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: RetailCore Developers',
+        'Licence :: OSI Approved :: Apache Software Licence',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ]
+)
